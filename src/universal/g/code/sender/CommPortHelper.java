1ce7046a0c42d4edca54aff30401a06adbe0eb26
Will Winder <Owen@crapbook.home>
2012-06-27 09:11:29 -0400

    Implemented close serial connection button. Added GUI open/close state changes (activating/deactivating fields).

--- src/universal/g/code/sender/CommPortHelper.java
+++ src/universal/g/code/sender/CommPortHelper.java
@@ -20,64 +20,97 @@ public class CommPortHelper {
     private CommPort commPort;
     private InputStream in;
     private OutputStream out;
-    JTextArea outputConsole;
+    private JTextArea outputConsole;
     
     // On OSX must run create /var/lock for some reason:
     // $ sudo mkdir /var/lock
     // $ sudo chmod 777 /var/lock
     boolean openCommPort(String name, int baud, JTextArea serialInputArea) {
-        outputConsole = serialInputArea;
+        this.outputConsole = serialInputArea;
+        boolean returnCode = false;
         try {
             CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(name);
             
             if (portIdentifier.isCurrentlyOwned()) {
-                return false;
+                returnCode = false;
             } else {
-                    commPort = portIdentifier.open(this.getClass().getName(), 2000);
+                    this.commPort = portIdentifier.open(this.getClass().getName(), 2000);
                     
-                    SerialPort serialPort = (SerialPort) commPort;
+                    SerialPort serialPort = (SerialPort) this.commPort;
                     serialPort.setSerialPortParams(baud,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
 
-                    in = serialPort.getInputStream();
-                    out = serialPort.getOutputStream();
+                    this.in = serialPort.getInputStream();
+                    this.out = serialPort.getOutputStream();
 
                     serialPort.addEventListener(new SerialReader(in, serialInputArea));
-                    serialPort.notifyOnDataAvailable(true);                    
+                    serialPort.notifyOnDataAvailable(true);  
+                    serialPort.notifyOnBreakInterrupt(true);
             }
             
-            return true;
+            returnCode = true;
         } catch (NoSuchPortException ex) {
+            returnCode = false;
             System.out.println("No such port exception.");
             ex.printStackTrace();
             //Logger.getLogger(CommPortHelper.class.getName()).log(Level.SEVERE, null, ex);
         } catch (PortInUseException ex) {
+            returnCode = false;
             System.out.println("Port in use exception.");
             ex.printStackTrace();
             //Logger.getLogger(CommPortHelper.class.getName()).log(Level.SEVERE, null, ex);
         } catch (UnsupportedCommOperationException ex) {
+            returnCode = false;
             System.out.println("Unsupported Comm Operation Exception exception.");
             ex.printStackTrace();
         } catch (IOException ex) {
+            returnCode = false;
             System.out.println("Unsupported Comm Operation Exception exception.");
             ex.printStackTrace();
         } catch (TooManyListenersException ex) {
+            returnCode = false;
             System.out.println("Too Many Listeners exception.");
             ex.printStackTrace();
         } 
 
-        return false;
+        return returnCode;
+    }
+    
+    void closeCommPort() {
+
+        SerialPort serialPort = (SerialPort) this.commPort;
+        serialPort.removeEventListener();
+        this.commPort.close();
+
     }
     
     void sendCommandToComm(String command) {
         
         String str = command;
         
-        System.out.println("Command sent to comm: '"+str+"'");
+        
+        System.out.println("Command sent to comm: '"+str.replaceAll("\\r\\n|\\r|\\n", "")+"'");
 
         // Write command to output stream.
         PrintStream printStream = new PrintStream(out);
         printStream.print(command);
         printStream.close();
+    }
+    
+    // TODO: Figure out how this thing can detect it is disconnected...
+    boolean isCommPortOpen() {
+        try {
+            CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(this.commPort.getName());
+            String owner = portIdentifier.getCurrentOwner();
+            String thisClass = this.getClass().getName();
+
+            //SerialPort serialPort = (SerialPort) this.commPort;
+            //serialPort.isRTS();
+            
+            return portIdentifier.isCurrentlyOwned() && owner.equals(thisClass);                    
+        } catch (NoSuchPortException ex) {
+        }
+        
+        return false;
     }
     
                 
@@ -119,7 +152,12 @@ public class CommPortHelper {
         
         public void serialEvent(SerialPortEvent arg0) {
             int data;
-          
+
+            if (arg0.getEventType() == SerialPortEvent.BI) {
+                String str = "**** Connection closed, serial reader exiting ****";
+                this.outputConsole.append(str + '\n');
+                System.exit(-1);
+            } else if (arg0.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
             try
             {
                 int len = 0;
@@ -131,14 +169,17 @@ public class CommPortHelper {
                     buffer[len++] = (byte) data;
                 }
 
-                outputConsole.append(new String(buffer,0,len) + '\n');
+                this.outputConsole.append(new String(buffer,0,len) + '\n');
             }
             catch ( IOException e )
             {
                 e.printStackTrace();
                 System.exit(-1);
-            }             
+            }
+            }
         }
+        
+        
     }
 
 }
