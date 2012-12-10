6cea9a288b93415302704c110d5f7e06333f3ea8
winder <wwinder.unh@gmail.com>
2012-12-10 17:05:02 -0500

    Added GPL notices and comments to the top of each source file.

--- src/com/willwinder/universalgcodesender/GcodeCommand.java
+++ src/com/willwinder/universalgcodesender/GcodeCommand.java
@@ -1,7 +1,30 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * An object representing a single GcodeCommand. The only tricky part about this
+ * class is the commandNum, which is used in the GUI for indexing a table of
+ * these objects. 
+ * 
+ * TODO: Get rid of the commandNum member.
  */
+
+/*
+    Copywrite 2012 Will Winder
+
+    This file is part of Universal Gcode Sender (UGS).
+
+    UGS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UGS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.willwinder.universalgcodesender;
 
 /**
