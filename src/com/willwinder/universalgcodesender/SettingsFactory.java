ce8e3513dbc90a32661ee0f0ebd81d3c8e72fe68
winder <wwinder.unh@gmail.com>
2013-01-28 07:55:26 -0500

    Added GPL notice to new file.

--- src/com/willwinder/universalgcodesender/SettingsFactory.java
+++ src/com/willwinder/universalgcodesender/SettingsFactory.java
@@ -1,7 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+    Copywrite 2013 Christian Moll
+
+    This file is part of Universal Gcode Sender (UGS).
+
+    UGS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UGS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.willwinder.universalgcodesender;
 
 import java.io.File;
@@ -15,7 +30,7 @@ import java.util.logging.Logger;
  * @author moll
  */
 class SettingsFactory {
-    private static File SETTINGS_FILE  = new File(System.getProperty("user.home"),".UniversalGcodeSender.properties");;
+    private static File SETTINGS_FILE  = new File(System.getProperty("user.home"),".UniversalGcodeSender.properties");
     
     private static Properties settings = new Properties();
     private static Logger logger = Logger.getLogger(SettingsFactory.class.getName());
