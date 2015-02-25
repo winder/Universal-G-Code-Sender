c4a9cd5a93618a71ba0165f6cf40fe7e8e803deb
winder <wwinder.unh@gmail.com>
2015-02-25 07:03:12 -0500

    Update version to 2.0

--- src/com/willwinder/universalgcodesender/Version.java
+++ src/com/willwinder/universalgcodesender/Version.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Version {
-    private static final String VERSION = "1.0.8 [nightly] ";
+    private static final String VERSION = "2.0 [nightly] ";
     private static String TIMESTAMP = "";
 
     private static boolean initialized = false;
