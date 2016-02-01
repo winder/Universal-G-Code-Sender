fb7448086bf5812399eb64588db1d384baa73b09
winder <wwinder.unh@gmail.com>
2016-02-01 13:19:05 -0500

    Shorter code for opening the editor.

--- UGSPlatform/GcodeEditor/src/com/willwinder/ugs/nbp/editor/EditGcodeFile.java
+++ UGSPlatform/GcodeEditor/src/com/willwinder/ugs/nbp/editor/EditGcodeFile.java
@@ -16,6 +16,7 @@ import javax.swing.Action;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.cookies.OpenCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
@@ -60,23 +61,14 @@ public final class EditGcodeFile implements ActionListener {
         BackendAPI backend = CentralLookup.getDefault().lookup(BackendAPI.class);
         if (backend == null || backend.getGcodeFile() == null) return;
 
+        // Close any opened file.
+        closeOpenFile();
+
         try {
-            // Close any opened file.
-            closeOpenFile();
-            
             FileObject fo = FileUtil.toFileObject(backend.getGcodeFile());
-            DataObject newDo = DataObject.find(fo);
-            final Node node = newDo.getNodeDelegate();
-            Action a = node.getPreferredAction();
-            if (a instanceof ContextAwareAction) {
-                a = ((ContextAwareAction) a).createContextAwareInstance(node.getLookup());
-            }
-            if (a != null) {
-                a.actionPerformed(new ActionEvent(node, ActionEvent.ACTION_PERFORMED, "")); // NOI18N
-            }
+            DataObject.find(fo).getLookup().lookup(OpenCookie.class).open();
         } catch (DataObjectNotFoundException ex) {
             Exceptions.printStackTrace(ex);
         }
-
     }
 }
