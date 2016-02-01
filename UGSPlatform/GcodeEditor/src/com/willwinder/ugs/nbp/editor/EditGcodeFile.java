9c0537ad392dfecaf3109895093992ff6284d428
winder <wwinder.unh@gmail.com>
2016-02-01 08:14:19 -0500

    Only open one editor at a time.

--- UGSPlatform/GcodeEditor/src/com/willwinder/ugs/nbp/editor/EditGcodeFile.java
+++ UGSPlatform/GcodeEditor/src/com/willwinder/ugs/nbp/editor/EditGcodeFile.java
@@ -9,23 +9,23 @@ import com.willwinder.ugs.nbp.lookup.CentralLookup;
 import com.willwinder.universalgcodesender.model.BackendAPI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import javax.swing.Action;
-import org.openide.*;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
-import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.Node;
-import org.openide.text.DataEditorSupport;
-import org.openide.text.DataEditorSupport.Env;
 import org.openide.util.*;
 import org.openide.util.NbBundle.Messages;
+import org.openide.windows.Mode;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
 
 @ActionID(
         category = "File",
@@ -37,11 +37,32 @@ import org.openide.util.NbBundle.Messages;
 @ActionReference(path = "Menu/File", position = 1301)
 @Messages("CTL_EditGcodeFile=Edit Gcode File...")
 public final class EditGcodeFile implements ActionListener {
+    private void closeOpenFile() {
+        Collection<TopComponent> editors = getCurrentlyOpenedEditors();
+        for (TopComponent editor : editors) {
+            editor.close();
+        }
+    }
+
+    private Collection<TopComponent> getCurrentlyOpenedEditors() {
+        final ArrayList<TopComponent> result = new ArrayList<TopComponent>();
+        final WindowManager wm = WindowManager.getDefault();
+        for (Mode mode : wm.getModes()) {
+            if (wm.isEditorMode(mode)) {
+                result.addAll(Arrays.asList(wm.getOpenedTopComponents(mode)));
+            }
+        }
+        return result;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        BackendAPI backend = CentralLookup.getDefault().lookup(BackendAPI.class);
+        if (backend == null || backend.getGcodeFile() == null) return;
+
         try {
-            BackendAPI backend = CentralLookup.getDefault().lookup(BackendAPI.class);
+            // Close any opened file.
+            closeOpenFile();
             
             FileObject fo = FileUtil.toFileObject(backend.getGcodeFile());
             DataObject newDo = DataObject.find(fo);
