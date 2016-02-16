33122bcfa9c336c34c80e0865341796ac7aaf3b3
winder <wwinder.unh@gmail.com>
2016-02-16 08:04:18 -0500

    Experimenting with Gcode mime type.

---
+++ UGSPlatform/GcodeEditor/src/com/willwinder/ugs/nbp/editor/type/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "GcodeTemplate.gcode")
+package com.willwinder.ugs.nbp.editor.type;
+
+import org.netbeans.api.templates.TemplateRegistration;
