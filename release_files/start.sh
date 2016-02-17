e0d7db6b6325852f07938126cfe8b4266eb06c3c
winder <wwinder.unh@gmail.com>
2016-02-16 21:22:36 -0500

    Fix linux start script with spaces. Closes #316

--- release_files/start.sh
+++ release_files/start.sh
@@ -6,20 +6,20 @@ unamestr=`uname`
 case "$unamestr" in
         Linux)
                 platform='linux'
-                rootdir="$(dirname $(readlink -f $0))"
+                rootdir=$(dirname "$(readlink -f $0)")
         ;;
         Darwin)
                 platform='mac'
-                rootdir="$(cd $(dirname $0); pwd -P)"
+                rootdir=$(cd "$(dirname $0)"; pwd -P)
         ;;
 esac
 
 case "$platform" in
         mac)
-                java -Xdock:name=UniversalGCodeSender -jar -Xmx256m $rootdir/UniversalGcodeSender*.jar
+                java -Xdock:name=UniversalGCodeSender -jar -Xmx256m "$rootdir"/UniversalGcodeSender*.jar
         ;;
         linux)
-                java -jar -Xmx256m $rootdir/UniversalGcodeSender*.jar
+                java -jar -Xmx256m "$rootdir"/UniversalGcodeSender*.jar
         ;;
 esac
         
