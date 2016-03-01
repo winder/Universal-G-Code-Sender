158340d4e8ce96a2822338e9c7288913458cdfa3
winder <wwinder.unh@gmail.com>
2016-03-01 17:39:01 -0500

    Make grid a bit nicer. #226

--- UGSPlatform/VisualizerModule/src/com/willwinder/ugs/nbm/visualizer/util/Grid.java
+++ UGSPlatform/VisualizerModule/src/com/willwinder/ugs/nbm/visualizer/util/Grid.java
@@ -36,38 +36,54 @@ public class Grid extends Renderable {
 
     @Override
     public void draw(GLAutoDrawable drawable, Point3d workCoord, Point3d focusMin, Point3d focusMax, double scaleFactor) {
+        double side = VisualizerUtils.findMaxSide(focusMin, focusMax) * 0.05;
+        Point3d bottomLeft = new Point3d(focusMin);
+        Point3d topRight = new Point3d(focusMax);
+
+        bottomLeft.x -= side;
+        bottomLeft.y -= side;
+        topRight.x += side;
+        topRight.y += side;
+
         GL2 gl = drawable.getGL().getGL2();
         gl.glPushMatrix();
             gl.glRotated(90, 1.0, 0.0, 0.0);
-            gl.glColor4f(.3f,.3f,.3f, .5f);
+            gl.glColor4f(.3f,.3f,.3f, .4f);
+
             // floor - cover entire model and a little extra.
-            double side = VisualizerUtils.findMaxSide(focusMin, focusMax) * 0.05;
             gl.glPushMatrix();
                 gl.glBegin(gl.GL_QUADS);
-                gl.glVertex3d(focusMin.x-side, 0,-focusMin.y+side);
-                gl.glVertex3d(focusMin.x-side, 0,-focusMax.y-side);
-                gl.glVertex3d(focusMax.x+side, 0,-focusMax.y-side);
-                gl.glVertex3d(focusMax.x+side, 0,-focusMin.y+side);
+                    gl.glVertex3d(bottomLeft.x, 0, -bottomLeft.y);
+                    gl.glVertex3d(bottomLeft.x, 0, -topRight.y);
+                    gl.glVertex3d(topRight.x  , 0, -topRight.y);
+                    gl.glVertex3d(topRight.x  , 0, -bottomLeft.y);
                 gl.glEnd();
             gl.glPopMatrix();
+            
+            double width = Math.abs(bottomLeft.x) + Math.abs(topRight.x);
+            double height = Math.abs(bottomLeft.y) + Math.abs(topRight.y);
 
+            double stepSize = Math.min(width/20, height/20);
             // grid
             gl.glBegin(GL_LINES);
-            for(double i=-side;i<=side;i++) {
-                if (i==0) { gl.glColor3d(.6f,.3f,.3f); } else { gl.glColor3d(.25,.25,.25); };
-                gl.glVertex3d(i,0.001,-side);
-                gl.glVertex3d(i,0.001,side);
-                gl.glVertex3d(i,-0.001,-side);
-                gl.glVertex3d(i,-0.001,side);
-                if (i==0) { gl.glColor3d(.3,.3,.6); } else { gl.glColor3d(.25,.25,.25); };
-                gl.glVertex3d(-side,0.001,i);
-                gl.glVertex3d(side,0.001,i);
-                gl.glVertex3d(-side,-0.001,i);
-                gl.glVertex3d(side,-0.001,i);
+            for(double x=bottomLeft.x;x<=topRight.x;x+=stepSize) {
+                for (double y=bottomLeft.y; y<=topRight.y; y+=stepSize) {
+                    if (x==0) { gl.glColor3d(.6f,.3f,.3f); } else { gl.glColor3d(.25,.25,.25); };
+                    gl.glVertex3d(x,  0.001, -bottomLeft.y);
+                    gl.glVertex3d(x,  0.001, -topRight.y);
+
+                    gl.glVertex3d(x, -0.001, -bottomLeft.y);
+                    gl.glVertex3d(x, -0.001, -topRight.y);
+                    
+                    if (x==0) { gl.glColor3d(.3,.3,.6); } else { gl.glColor3d(.25,.25,.25); };
+                    gl.glVertex3d(bottomLeft.x,  0.001, -y);
+                    gl.glVertex3d(topRight.x  ,  0.001, -y);
+
+                    gl.glVertex3d(bottomLeft.x, -0.001, -y);
+                    gl.glVertex3d(topRight.x  , -0.001, -y);
+                }
             };
             gl.glEnd();
-
         gl.glPopMatrix();
     }
-    
 }
