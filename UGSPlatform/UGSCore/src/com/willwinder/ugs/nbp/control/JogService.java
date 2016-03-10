bffb9958067bb436edaec9793231c209703bf048
winder <wwinder.unh@gmail.com>
2016-03-10 17:48:28 -0500

    Couldn't get these dynamic actions to register with the KeyMap configuration tool.

--- UGSPlatform/UGSCore/src/com/willwinder/ugs/nbp/control/JogService.java
+++ UGSPlatform/UGSCore/src/com/willwinder/ugs/nbp/control/JogService.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Copywrite 2016 Will Winder
+
+    This file is part of Universal Gcode Sender (UGS).
+
+    UGS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UGS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.willwinder.ugs.nbp.control;
 
@@ -26,6 +39,7 @@ public class JogService {
 
     public JogService() {
         backend = CentralLookup.getDefault().lookup(BackendAPI.class);
+        //initActions();
     }
 
     public void setStepSize(double size) {
@@ -48,4 +62,61 @@ public class JogService {
     public boolean canJog() {
         return backend.getControlState() == UGSEvent.ControlState.COMM_IDLE;
     }
+
+    /**
+     * Create the jog actions.
+     */
+    /*
+    private void initActions() {
+        SwingUtilities.invokeLater(() -> {
+            FileObject menuFolder = FileUtil.getConfigFile("Menu/Machine/Jog");
+            try {
+                registerAction("JogService.xPlus" , menuFolder, 1, 0, 0);
+                registerAction("JogService.xMinus", menuFolder,-1, 0, 0);
+                registerAction("JogService.yPlus" , menuFolder, 0, 1, 0);
+                registerAction("JogService.yMinus", menuFolder, 0,-1, 0);
+                registerAction("JogService.zPlus" , menuFolder, 0, 0, 1);
+                registerAction("JogService.zMinus", menuFolder, 0, 0,-1);
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        });
+    }
+
+    private void registerAction(String nameId, FileObject in, int x, int y, int z) throws IOException {
+        String name = NbBundle.getMessage(JogService.class, nameId);
+
+        // Create if missing.
+        FileObject menu = in.getFileObject(name, "instance");
+        if (menu == null) {
+            menu = in.createData(name, "instance");
+        }
+
+        AbstractAction action = new JogAction(name, this, x, y, z);
+        menu.setAttribute("instanceCreate", action);
+        menu.setAttribute("instanceClass", action.getClass().getName());
+    }
+
+    protected class JogAction extends AbstractAction {
+        JogService js;
+        int x,y,z;
+        public JogAction(String name, JogService service, int x, int y, int z) {
+            super(name);
+            js = service;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            js.adjustManualLocation(x, y, z);
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return js.canJog();
+        }
+    }
+    */
 }
