6d3a35d6f4bd097a35e0b983f4aafd9ddc9d0ac1
winder <wwinder.unh@gmail.com>
2016-03-20 18:56:47 -0400

    Populate visualizer options.

--- UGSPlatform/VisualizerModule/src/com/willwinder/ugs/nbm/visualizer/options/VisualizerOptionsPanel.java
+++ UGSPlatform/VisualizerModule/src/com/willwinder/ugs/nbm/visualizer/options/VisualizerOptionsPanel.java
@@ -20,12 +20,44 @@ package com.willwinder.ugs.nbm.visualizer.options;
 
 import com.willwinder.ugs.nbp.lib.options.AbstractOptionsPanel;
 import com.willwinder.ugs.nbp.lib.options.IChanged;
+import com.willwinder.ugs.nbp.lib.options.OptionTable.Option;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import org.openide.util.NbPreferences;
 
 final class VisualizerOptionsPanel extends AbstractOptionsPanel {
+    private Collection<colorPref> colorPreferences;
+
+    private class colorPref {
+        public String preference;
+        public String localized;
+        public Color defaultColor;
+        public colorPref(String p, String l, Color c) {
+            preference = p;
+            localized = l;
+            defaultColor = c;
+        }
+    }
 
     public VisualizerOptionsPanel(IChanged changer) {
         super(changer);
-        // TODO listen to changes in form fields and call controller.changed()
+        colorPreferences = new ArrayList<>();
+
+        colorPreferences.add(new colorPref("vizualizer.color.background", "Background Color", new Color(220,235,255)));
+        colorPreferences.add(new colorPref("vizualizer.color.tool", "Tool Color", new Color(237,255,0)));
+        colorPreferences.add(new colorPref("vizualizer.color.linear", "Linear Movement Color (G1)", new Color(0,0,158)));
+        colorPreferences.add(new colorPref("vizualizer.color.rapid", "Rapid Movement Color (G0)", new Color(204,204,0)));
+        colorPreferences.add(new colorPref("vizualizer.color.arc", "Arc Movement Color (G2/G3)", new Color(178,34,34)));
+        colorPreferences.add(new colorPref("vizualizer.color.plunge", "Plunge/Raise Movement Color", new Color(0,100,0)));
+        colorPreferences.add(new colorPref("vizializer.color.highlight", "Editor Line Highlight Color", new Color(237,255,0)));
+    }
+
+    private Color getColorOption(String option, Color defaultColor) {
+        int pref = NbPreferences.forModule(VisualizerOptionsPanel.class).getInt(option, defaultColor.getRGB());
+        return new Color(pref, true);
     }
 
     @Override
@@ -37,6 +69,11 @@ final class VisualizerOptionsPanel extends AbstractOptionsPanel {
         // someCheckBox.setSelected(NbPreferences.forModule(VisualizerPanel.class).getBoolean("someFlag", false));
         // or:
         // someTextField.setText(SomeSystemOption.getDefault().getSomeStringProperty());
+
+        for (colorPref cp : colorPreferences) {
+            Color c = getColorOption(cp.preference, cp.defaultColor);
+            this.add(new Option<>(cp.localized, "", c));
+        }
     }
 
     @Override
