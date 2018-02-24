2fd944ac41cd5a5c77e8c0a4a7aabe409e1358af
winder <wwinder.unh@gmail.com>
2018-02-24 13:16:07 -0500

    The supporting code for this is gone.

--- ugs-core/src/com/willwinder/universalgcodesender/model/GUIBackend.java
+++ ugs-core/src/com/willwinder/universalgcodesender/model/GUIBackend.java
@@ -267,28 +267,6 @@ public class GUIBackend implements BackendAPI, ControllerListener, SettingChange
             return;
         }
 
-        // This breaks when a machine is homing. GRBL at least will stop sending
-        // status during a homing operation.
-        /*
-        // Check if a timeout has occurred.
-        if (controller.getStatusUpdatesEnabled() && settings.isAutoReconnect()) {
-            long now = System.currentTimeMillis();
-            if (now - lastResponse > AUTO_DISCONNECT_THRESHOLD &&
-                    now - lastConnectAttempt > AUTO_DISCONNECT_THRESHOLD ) {
-                logger.log(Level.INFO, "No Response in " + (now - lastResponse)+"ms.");
-                if (controller != null && controller.isStreamingFile()) {
-                    streamFailed = true;
-                }
-
-                try {
-                    disconnectInternal();
-                } catch (Exception e) {
-                    logger.log(Level.INFO, "Disconnect failed ", e);
-                }
-            }
-        }
-        */
-
         if (!isConnected()) {
             if (settings == null || streamFailed) {
                 return;
