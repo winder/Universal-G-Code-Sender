7713d1bb8eaecf78a3f81a65b313bd57bd8b9ad6
Joacim Breiler <joacim@breiler.com>
2018-04-03 16:22:05 +0200

    Added a test for checking the controller state.

--- ugs-core/test/com/willwinder/universalgcodesender/GrblUtilsTest.java
+++ ugs-core/test/com/willwinder/universalgcodesender/GrblUtilsTest.java
@@ -18,6 +18,7 @@
  */
 package com.willwinder.universalgcodesender;
 
+import com.willwinder.universalgcodesender.listeners.ControllerState;
 import com.willwinder.universalgcodesender.listeners.ControllerStatus;
 import com.willwinder.universalgcodesender.model.Position;
 import com.willwinder.universalgcodesender.model.UnitUtils;
@@ -434,6 +435,7 @@ public class GrblUtilsTest {
         ControllerStatus controllerStatus = GrblUtils.getStatusFromStatusString(null, status, version, unit);
 
         assertEquals("Idle", controllerStatus.getStateString());
+        assertEquals(ControllerState.IDLE, controllerStatus.getState());
 
         assertEquals(new Position(1.1,2.2,3.3, UnitUtils.Units.MM), controllerStatus.getMachineCoord());
         assertEquals(new Position(4.4,5.5,6.6, UnitUtils.Units.MM), controllerStatus.getWorkCoord());
