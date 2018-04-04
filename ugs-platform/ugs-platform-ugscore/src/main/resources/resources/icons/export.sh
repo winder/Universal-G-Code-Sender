994cc75bbab0673dca808fb15c7052025cad9bd9
winder <wwinder.unh@gmail.com>
2018-04-04 10:33:21 -0400

    Update export script to generate _dark and _disabled_dark svg files.

--- ugs-platform/ugs-platform-ugscore/src/main/resources/resources/icons/export.sh
+++ ugs-platform/ugs-platform-ugscore/src/main/resources/resources/icons/export.sh
@@ -4,7 +4,20 @@ if [ -f /Applications/Inkscape.app/Contents/Resources/bin/inkscape ]; then
 	alias inkscape="/Applications/Inkscape.app/Contents/Resources/bin/inkscape"
 fi
 
+# Make sure nobody is using black instead of blackish
+for INPUT in *.svg ; do 
+  sed -e "s/#000000/#353a40/" "${PWD}/${INPUT}" > "${PWD}/temp"
+  mv  "${PWD}/temp" "${PWD}/${INPUT}"
+done
 
+# Generate _dark and _disabled_dark vectors
+for INPUT in *.svg ; do 
+	FILENAME=${INPUT%.*}
+  sed -e "s/#353a40/#0ac9f1/" "${PWD}/${INPUT}" > "${PWD}/${FILENAME}_dark.svg"
+  sed -e "s/#353a40/#41818f/" "${PWD}/${INPUT}" > "${PWD}/${FILENAME}_disabled_dark.svg"
+done
+
+# Generage png files in various sizes
 for INPUT in *.svg ; do 
 	FILENAME=${INPUT%.*}
 	SUFFIX=".png"
@@ -16,7 +29,7 @@ for INPUT in *.svg ; do
 		SUFFIX="_dark.png"
 	fi
 
-	inkscape --export-area-page --file ${PWD}/${INPUT} -w 16 -h 16 --export-png ${PWD}/${FILENAME}${SUFFIX}
-	inkscape --export-area-page --file ${PWD}/${INPUT} -w 24 -h 24 --export-png ${PWD}/${FILENAME}24${SUFFIX}
-	inkscape --export-area-page --file ${PWD}/${INPUT} -w 32 -h 32 --export-png ${PWD}/${FILENAME}32${SUFFIX}
+	inkscape --export-area-page --file "${PWD}/${INPUT}" -w 16 -h 16 --export-png "${PWD}/${FILENAME}${SUFFIX}"
+	inkscape --export-area-page --file "${PWD}/${INPUT}" -w 24 -h 24 --export-png "${PWD}/${FILENAME}24${SUFFIX}"
+	inkscape --export-area-page --file "${PWD}/${INPUT}" -w 32 -h 32 --export-png "${PWD}/${FILENAME}32${SUFFIX}"
 done
