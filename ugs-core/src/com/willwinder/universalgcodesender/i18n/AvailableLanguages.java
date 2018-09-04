9b60aceccad83a20eb40e416a517971ec72bf5dd
winder <wwinder.unh@gmail.com>
2018-09-04 15:09:24 -0400

    Add Norwegian Nynorsk as a selectable language in the GUI.

--- ugs-core/src/com/willwinder/universalgcodesender/i18n/AvailableLanguages.java
+++ ugs-core/src/com/willwinder/universalgcodesender/i18n/AvailableLanguages.java
@@ -45,6 +45,7 @@ public class AvailableLanguages {
         availableLanguages.add(new Language("tr", "TR", "Turkish"));
         availableLanguages.add(new Language("lt", "LT", "Lithuanian"));
         availableLanguages.add(new Language("nb", "NO", "Norwegian Bokmål"));
+        availableLanguages.add(new Language("nn", "NO", "Norwegian Nynorsk"));
         availableLanguages.add(new Language("pl", "PL", "Polish"));
     }
 
