6a74e96b1e23f42a26fb85de317d4fdd5cb73f30
Will Winder <wwinder.unh@gmail.com>
2019-05-05 12:12:12 -0400

    Remove debug line.

--- scripts/package_and_deploy.sh
+++ scripts/package_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -xe
-echo "$ARTIFACTORY_NAME"
+
 ./jfrog rt config --interactive=false --url "$ARTIFACTORY_URL" --user "$ARTIFACTORY_LOGIN" --apikey "$ARTIFACTORY_API_KEY"
 ./jfrog rt u "ugs-classic/target/UniversalGcodeSender.jar" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
