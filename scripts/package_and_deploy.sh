ec749b5af8021b4ca3c401c5adc380503bf8c8d4
Will Winder <wwinder.unh@gmail.com>
2019-05-05 11:50:25 -0400

    Make deploy script more verbose.

--- scripts/package_and_deploy.sh
+++ scripts/package_and_deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -xe
 "./jfrog rt config --interactive=false --url $ARTIFACTORY_URL --user $ARTIFACTORY_LOGIN --apikey $ARTIFACTORY_API_KEY"
 ./jfrog rt u "ugs-core/target/UniversalGcodeSender.jar" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER --flat=true
 ./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER --flat=true
