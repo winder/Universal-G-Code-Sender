a65112c092d0ec8390f853340fd2404900847767
Will Winder <wwinder.unh@gmail.com>
2019-05-05 12:52:00 -0400

    Disable broken OSX installer, don't use after_script which seems to run AFTER the deploy step which isn't useful.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@
 set -xe
 
 ./jfrog rt config --interactive=false --url "$ARTIFACTORY_URL" --user "$ARTIFACTORY_LOGIN" --apikey "$ARTIFACTORY_API_KEY"
-./jfrog rt u "ugs-classic/target/UniversalGcodeSender.jar" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
+./jfrog rt u "ugs-classic/target/UniversalGcodeSender.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.dmg" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
-#./jfrog rt u "ugs-platform/application/target/site/netbeans_site/" "/UGS/update-center/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER
+./jfrog rt u "ugs-platform/application/target/site/netbeans_site/" "/UGS/update-center/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER
 ./jfrog rt bce $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER
 ./jfrog rt bp $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER
