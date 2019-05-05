074bea0dc1367fd0834a838738e9c6b31599b5b8
Will Winder <wwinder.unh@gmail.com>
2019-05-05 11:57:56 -0400

    More build script testing...

--- scripts/package_and_deploy.sh
+++ scripts/package_and_deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 set -xe
-"./jfrog rt config --interactive=false --url $ARTIFACTORY_URL --user $ARTIFACTORY_LOGIN --apikey $ARTIFACTORY_API_KEY"
-./jfrog rt u "ugs-core/target/UniversalGcodeSender.jar" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER --flat=true
-./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER --flat=true
-./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.dmg" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER --flat=true
+echo "$ARTIFACTORY_NAME"
+./jfrog rt config --interactive=false --url "$ARTIFACTORY_URL" --user "$ARTIFACTORY_LOGIN" --apikey "$ARTIFACTORY_API_KEY"
+./jfrog rt u "ugs-core/target/UniversalGcodeSender.jar" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
+./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
+./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.dmg" "/UGS/dist/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/site/netbeans_site/" "/UGS/update-center/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER
-"./jfrog rt bce $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER"
-"./jfrog rt bp $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER"
+./jfrog rt bce $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER
+./jfrog rt bp $ARTIFACTORY_NAME $TRAVIS_BUILD_NUMBER
