4511f85b349bcc318837a90b25b3c3d19308181d
Joacim Breiler <joacim@breiler.com>
2019-08-02 07:33:29 +0200

    Fixed problem where the remain duration is calculated with negative time when a stream is stopped.

--- ugs-core/src/com/willwinder/universalgcodesender/model/GUIBackend.java
+++ ugs-core/src/com/willwinder/universalgcodesender/model/GUIBackend.java
@@ -541,13 +541,14 @@ public class GUIBackend implements BackendAPI, ControllerListener, SettingChange
     @Override
     public long getSendRemainingDuration() {
         long completedRows = getNumCompletedRows();
+        long numberOfRows = getNumRows();
 
         // Early exit condition. Can't make an estimate if we haven't started.
-        if (completedRows == 0) { return -1L; }
+        if (completedRows == 0 || numberOfRows == 0) { return -1L; }
 
         long elapsedTime = getSendDuration();
         long timePerRow = elapsedTime / completedRows;
-        long estimate = getNumRows() * timePerRow;
+        long estimate = numberOfRows * timePerRow;
         return estimate - elapsedTime;
     }
 
