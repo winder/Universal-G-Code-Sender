4426f71c900ddd4a1eeb3957f3275cedc6312534
Joacim Breiler <joacim@breiler.com>
2020-12-04 18:21:41 +0100

    Fixed problem with tiler plugin not rounding decimals (#1484)
    * Fixed problem with tiler plugin not rounding decimals
    
    * Forgot to fix a unit test

--- ugs-core/test/com/willwinder/universalgcodesender/gcode/processors/TranslatorTest.java
+++ ugs-core/test/com/willwinder/universalgcodesender/gcode/processors/TranslatorTest.java
@@ -30,85 +30,96 @@ import org.junit.Test;
 import java.util.List;
 
 /**
- *
  * @author wwinder
  */
 public class TranslatorTest {
-  private Translator instance;
-  private GcodeState state;
+    private Translator instance;
+    private GcodeState state;
 
-  // In case it's needed...
-  private DecimalProcessor truncator = new DecimalProcessor(4);
+    // In case it's needed...
+    private DecimalProcessor truncator = new DecimalProcessor(4);
 
-  @Before
-  public void setUp() {
-      instance = new Translator(new Position(10.1, 20.2, 30.3, Units.MM));
+    @Before
+    public void setUp() {
+        instance = new Translator(new Position(10.1, 20.2, 30.3, Units.MM));
 
-      state = new GcodeState();
-      state.units = Code.G21;
-      state.inAbsoluteMode = true;
-      state.inAbsoluteIJKMode = false;
-      state.currentPoint = null;
-  }
+        state = new GcodeState();
+        state.units = Code.G21;
+        state.inAbsoluteMode = true;
+        state.inAbsoluteIJKMode = false;
+        state.currentPoint = null;
+    }
 
-  @Test
-  public void basicTranslate() throws Exception {
-      List<String> result = instance.processCommand("G0 X0 Y0 Z0", state);
+    @Test
+    public void basicTranslate() {
+        List<String> result = instance.processCommand("G0 X0 Y0 Z0", state);
 
-      Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3");
+    }
 
-  @Test
-  public void basicTranslateNegative() throws Exception {
-      instance = new Translator(new Position(-10.1, -20.2, -30.3, Units.MM));
+    @Test
+    public void basicTranslateNegative() {
+        instance = new Translator(new Position(-10.1, -20.2, -30.3, Units.MM));
 
-      List<String> result = instance.processCommand("G0 X10 Y20 Z30", state);
+        List<String> result = instance.processCommand("G0 X10 Y20 Z30", state);
 
-      Assertions.assertThat(result).hasSize(1);
-      result = truncator.processCommand(result.get(0), state);
-      Assertions.assertThat(result).hasSize(1).contains("G0X-0.1Y-0.2Z-0.3");
-  }
+        Assertions.assertThat(result).hasSize(1);
+        result = truncator.processCommand(result.get(0), state);
+        Assertions.assertThat(result).hasSize(1).contains("G0X-0.1Y-0.2Z-0.3");
+    }
 
-  @Test
-  public void arcTranslate() throws Exception {
-      List<String> result = instance.processCommand("G3 X0 Y0 Z0 I0.5 J0.5", state);
+    @Test
+    public void arcTranslate() {
+        List<String> result = instance.processCommand("G3 X0 Y0 Z0 I0.5 J0.5", state);
 
-      Assertions.assertThat(result).hasSize(1).contains("G3X10.1Y20.2Z30.3I0.5J0.5");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G3X10.1Y20.2Z30.3I0.5J0.5");
+    }
 
-  @Test
-  public void arcTranslateAbsoluteIJK() throws Exception {
-      state.inAbsoluteIJKMode = true;
+    @Test
+    public void arcTranslateAbsoluteIJK() {
+        state.inAbsoluteIJKMode = true;
 
-      List<String> result = instance.processCommand("G3 X0 Y0 Z0 I0.5 J0.5", state);
+        List<String> result = instance.processCommand("G3 X0 Y0 Z0 I0.5 J0.5", state);
 
-      Assertions.assertThat(result).hasSize(1).contains("G3X10.1Y20.2Z30.3I10.6J20.7");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G3X10.1Y20.2Z30.3I10.6J20.7");
+    }
 
-  @Test
-  public void convertUnits() throws Exception {
-    // Offset is in MM, convert it to INCH
-    state.units = Code.G20;
+    @Test
+    public void convertUnits() {
+        // Offset is in MM, convert it to INCH
+        state.units = Code.G20;
 
-    List<String> result = instance.processCommand("G0 X0 Y0 Z0", state);
-    Assertions.assertThat(result).hasSize(1);
+        List<String> result = instance.processCommand("G0 X0 Y0 Z0", state);
+        Assertions.assertThat(result).hasSize(1);
 
-    result = truncator.processCommand(result.get(0), state);
+        result = truncator.processCommand(result.get(0), state);
 
-    Assertions.assertThat(result).hasSize(1).contains("G0X0.3976Y0.7953Z1.1929");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G0X0.398Y0.795Z1.193");
+    }
 
-  @Test
-  public void handlesComments1() throws Exception {
-      List<String> result = instance.processCommand("G0 X0 Y0 Z0 ; some comment", state);
+    @Test
+    public void handlesComments1() {
+        List<String> result = instance.processCommand("G0 X0 Y0 Z0 ; some comment", state);
 
-      Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3( some comment)");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3( some comment)");
+    }
 
-  @Test
-  public void handlesComments2() throws Exception {
-      List<String> result = instance.processCommand("(another comment)G0 X0 Y0 Z0", state);
+    @Test
+    public void handlesComments2() {
+        List<String> result = instance.processCommand("(another comment)G0 X0 Y0 Z0", state);
 
-      Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3(another comment)");
-  }
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.1Y20.2Z30.3(another comment)");
+    }
+
+    @Test
+    public void shouldRoundDecimals() {
+        List<String> result = instance.processCommand("G0 X0.0001", state);
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.1");
+
+        result = instance.processCommand("G0 X0.0009", state);
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.101");
+
+        result = instance.processCommand("G0 X0.001", state);
+        Assertions.assertThat(result).hasSize(1).contains("G0X10.101");
+    }
 }
