58bd2ca7fbb840f1b88e293a26d46bf59d4f4f13
Joacim Breiler <joacim@breiler.com>
2021-01-27 21:01:42 +0100

    Added a simple UndoManager implementation

--- ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/actions/SimpleUndoManager.java
+++ ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/actions/SimpleUndoManager.java
@@ -7,70 +7,73 @@ import java.util.Stack;
  * mechanism. UndoableActions can be added in the manager, which gives a
  * centered interface for performing their undo and redo actions.
  */
-public class UndoManager {
+public class SimpleUndoManager implements UndoManager {
 
-	// Undo and redo stacks which contain the UndoableAction objects
-	// When a new action is made it is put in the undo stack. When an operation
-	// is undone, it is places in the redo stack.
+    // Undo and redo stacks which contain the UndoableAction objects
+    // When a new action is made it is put in the undo stack. When an operation
+    // is undone, it is places in the redo stack.
 
-	private Stack<DrawAction> undoStack;
-	private Stack<DrawAction> redoStack;
+    private Stack<DrawAction> undoStack;
+    private Stack<DrawAction> redoStack;
 
-	/**
-	 * Constructs a empty Undo Manager.
-	 */
-	public UndoManager() {
-		this.undoStack = new Stack<>();
-		this.redoStack = new Stack<>();
-	}
+    /**
+     * Constructs a empty Undo Manager.
+     */
+    public SimpleUndoManager() {
+        this.undoStack = new Stack<>();
+        this.redoStack = new Stack<>();
+    }
 
-	/**
-	 * Adds a new undoable action into this Undo Manager.
-	 * 
-	 * @param action
-	 *            the UndoableAction to be added.
-	 */
-	public void addAction(DrawAction action) {
-		this.redoStack.clear();
-		this.undoStack.push(action);
-	}
+    /**
+     * Adds a new undoable action into this Undo Manager.
+     *
+     * @param action the UndoableAction to be added.
+     */
+    @Override
+    public void addAction(DrawAction action) {
+        this.redoStack.clear();
+        this.undoStack.push(action);
+    }
 
-	/**
-	 * Tests if an redo operation can be performed at this moment.
-	 * 
-	 * @return boolean value telling if a redo is possible to perform.
-	 */
+    /**
+     * Tests if an redo operation can be performed at this moment.
+     *
+     * @return boolean value telling if a redo is possible to perform.
+     */
+    @Override
+    public boolean canRedo() {
+        return !this.redoStack.isEmpty();
+    }
 
-	public boolean canRedo() {
-		return !this.redoStack.isEmpty();
-	}
+    /**
+     * Tests if an undo operation can be performed at this moment.
+     *
+     * @return boolean value telling if an undo is possible to perform.
+     */
+    @Override
+    public boolean canUndo() {
+        return !this.undoStack.isEmpty();
+    }
 
-	/**
-	 * Tests if an undo operation can be performed at this moment.
-	 * 
-	 * @return boolean value telling if an undo is possible to perform.
-	 */
-	public boolean canUndo() {
-		return !this.undoStack.isEmpty();
-	}
+    /**
+     * Redoes one action from the redo stack. The redone action then is moved to
+     * the undo stack.
+     */
+    @Override
+    public void redo() {
+        DrawAction action = this.redoStack.pop();
+        action.redo();
+        this.undoStack.push(action);
+    }
 
-	/**
-	 * Redoes one action from the redo stack. The redone action then is moved to
-	 * the undo stack.
-	 */
-	public void redo() {
-		DrawAction action = this.redoStack.pop();
-		action.redo();
-		this.undoStack.push(action);
-	}
-
-	/**
-	 * Undoes one action from the undo stack. The undone action then is moved to
-	 * the undo stack.
-	 */
-	public void undo() {
-		DrawAction action = this.undoStack.pop();
-		action.undo();
-		this.redoStack.push(action);
-	}
+    /**
+     * Undoes one action from the undo stack. The undone action then is moved to
+     * the undo stack.
+     */
+    @Override
+    public void undo() {
+        DrawAction action = this.undoStack.pop();
+        action.undo();
+        this.redoStack.push(action);
+    }
 }
