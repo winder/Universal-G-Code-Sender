76f984b4ae753eeba40411df1e8871d3cb65299e
AndyC <andy@corteen.org>
2021-07-22 15:38:20 +0100

    Implements #1158 Enhancing PatternRemover to PatternReplacer with sed syntax and %macro% expansion within the Replacer

--- ugs-core/src/com/willwinder/universalgcodesender/gcode/processors/PatternRemover.java
+++ ugs-core/src/com/willwinder/universalgcodesender/gcode/processors/PatternRemover.java
@@ -21,21 +21,28 @@
  */
 package com.willwinder.universalgcodesender.gcode.processors;
 
+
 import com.willwinder.universalgcodesender.gcode.GcodeState;
 import com.willwinder.universalgcodesender.i18n.Localization;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import com.willwinder.universalgcodesender.types.Macro;
+import com.willwinder.universalgcodesender.model.BackendAPI;
+import com.willwinder.ugs.nbp.lib.lookup.CentralLookup;
+import java.util.regex.Matcher;
+
 /**
  *
- * @author wwinder
+ * @author wwinder, AndyCXL
  */
 public class PatternRemover implements CommandProcessor {
     final private Pattern p;
-    // r[0] is "" or 'replace with' string from regexpattern supplied
-    final private List<String> r = new ArrayList<String>();
-    //private String r = "";
+    // r[0] is "" or 'replace with' string from regexpattern or sed supplied
+    private List<String> r = new ArrayList<>();
+    
+    private BackendAPI backend;
      
     public PatternRemover(String regexPattern) {
         /* AndyCXL enhancing 'remover' into 'remover or replacer' by
@@ -46,26 +53,48 @@ public class PatternRemover implements CommandProcessor {
         *  If the pattern is anything else, ie: not starting with " s/ " then
         *  intepret the whole pattern as grep and force replace the return string
         *  with the empty string ""
+        *  TODO: Parse macro names and replace regex/sed match with macro expansion
+        *  following conventions would suggest %macroname% as syntax
         */
         String[] s3 = regexPattern.split("/", 3);
         
         if (s3[0].trim().equals("s")) {
             // sed pattern identified, second split is therefore grep pattern
             p = Pattern.compile(s3[1].trim());
-            // Third split is the 'replace with' string, if it exists. Lazy pattern
-            // entry by user might omit the final / and string in the sed syntax
+            
+            // Third split is the 'replace with' string, use "" if absent
             if (s3.length == 3) {
+                // Does s3[2] contain a known macro name (one)? Expand if so
+                Pattern pm = Pattern.compile("%.+%");
+                Matcher mp = pm.matcher(s3[2]);
+                int matched = 0;
+                // Retrieve and match macros, expand macro.gcode() if defined
+                if (mp.matches()) {
+                    // Get the backend, through which macros are retrieved
+                    backend = CentralLookup.getDefault().lookup(BackendAPI.class);
+                    List<Macro> macros = backend.getSettings().getMacros();
+                    // Enumerate macros to find match
+                    for (Macro macro: macros) {
+                        // Iterate macros and test given name amongst macro names
+                        if (mp.group().equals("%"+macro.getName()+"%")) {
+                            s3[2] = s3[2].replace(mp.group(), macro.getGcode());
+                            matched = 1;
+                            break;
+                        }
+                    }
+                    if (matched == 0) {
+                        s3[2] = "";
+                    }
+                }
+                // Any macros are expanded, string is ready to submit
                 r.add(s3[2].trim());
-                //r = s3[2];
             } else {
                 r.add("");
-                //r = "";
             }
         } else {
             // grep pattern received, presume entire pattern is the regexPattern
             p = Pattern.compile(regexPattern);
             r.add("");
-            //r = "";
         }
     }
     
@@ -85,7 +114,6 @@ public class PatternRemover implements CommandProcessor {
         */
         //ret.add(p.matcher(command).replaceAll(""));
         ret.add( p.matcher(command).replaceAll( r.get(0) ) );
-        //System.out.println( "Command sed:" + command + "->-" + r.get(0) );
         return ret;
     }
 }
