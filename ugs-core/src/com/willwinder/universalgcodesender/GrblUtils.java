cd7504402ec6d6f0c2d0ecd0d8f1ff6776839b53
AndyC <andy@corteen.org>
2021-08-26 13:50:11 +0100

    Update GrblUtils.java

--- ugs-core/src/com/willwinder/universalgcodesender/GrblUtils.java
+++ ugs-core/src/com/willwinder/universalgcodesender/GrblUtils.java
@@ -561,7 +561,9 @@ public class GrblUtils {
                 result.b = Double.parseDouble(matcher.group(5));
             }
             if (matcher.group(4) != null) {
-                result.a = Double.parseDouble(matcher.group(4));
+                // Dirty hack for Grbl Mega 5X XYZYA axis shift
+                result.a = Double.parseDouble(matcher.group(5));
+                //result.a = Double.parseDouble(matcher.group(4));
             }
 
             return result;
