58bcf38e1fca179a2f3e5ca6499c2f2a30663d75
Joacim Breiler <joacim@breiler.com>
2022-01-09 15:27:52 +0100

    Fixed broken test

--- ugs-platform/ugs-platform-gcode-editor/src/test/java/com/willwinder/ugs/nbp/editor/parser/errors/InvalidGrblCommandErrorParserTest.java
+++ ugs-platform/ugs-platform-gcode-editor/src/test/java/com/willwinder/ugs/nbp/editor/parser/errors/InvalidGrblCommandErrorParserTest.java
@@ -26,6 +26,7 @@ public class InvalidGrblCommandErrorParserTest {
         BackendAPI backendAPI = mock(BackendAPI.class);
         IController grblController = mock(GrblController.class);
         when(backendAPI.getController()).thenReturn(grblController);
+        when(backendAPI.isConnected()).thenReturn(true);
 
         parser = new InvalidGrblCommandErrorParser(fileObject, backendAPI);
     }
