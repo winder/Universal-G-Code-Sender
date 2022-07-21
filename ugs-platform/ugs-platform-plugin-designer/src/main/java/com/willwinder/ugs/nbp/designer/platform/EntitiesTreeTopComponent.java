247973485dbb20fd018f69ee5d5fd478bb84c7e5
Joacim Breiler <joacim@breiler.com>
2022-07-21 16:05:56 +0200

    Fixed problem with the entity tree not expanding its rows when starting the designer. Now also closes related top components when the designer is closed. (#1938)

--- ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/platform/EntitiesTreeTopComponent.java
+++ ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/platform/EntitiesTreeTopComponent.java
@@ -19,11 +19,16 @@
 package com.willwinder.ugs.nbp.designer.platform;
 
 import com.willwinder.ugs.nbp.designer.gui.tree.EntitiesTree;
+import com.willwinder.ugs.nbp.designer.gui.tree.EntityTreeModel;
 import com.willwinder.ugs.nbp.designer.logic.Controller;
+import com.willwinder.ugs.nbp.designer.logic.ControllerEventType;
+import com.willwinder.ugs.nbp.designer.logic.ControllerListener;
 import com.willwinder.ugs.nbp.lib.lookup.CentralLookup;
+import net.miginfocom.swing.MigLayout;
 import org.openide.windows.TopComponent;
 
-import javax.swing.*;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
 
 /**
  * @author Joacim Breiler
@@ -33,24 +38,43 @@ import javax.swing.*;
         persistenceType = TopComponent.PERSISTENCE_NEVER
 )
 @TopComponent.Registration(mode = "bottom_left", openAtStartup = false)
-public class EntitiesTreeTopComponent extends TopComponent {
+public class EntitiesTreeTopComponent extends TopComponent implements ControllerListener {
     private static final long serialVersionUID = 432423498723987873L;
-    private transient final EntitiesTree entitesTree;
+    private transient EntitiesTree entitesTree;
+    private transient EntityTreeModel entitiesTreeModel;
 
     public EntitiesTreeTopComponent() {
         setMinimumSize(new java.awt.Dimension(50, 50));
         setPreferredSize(new java.awt.Dimension(200, 200));
         setLayout(new java.awt.BorderLayout());
         setDisplayName("Design objects");
+    }
 
-        Controller controller = CentralLookup.getDefault().lookup(Controller.class);
-        entitesTree = new EntitiesTree(controller);
+    @Override
+    protected void componentClosed() {
+        entitesTree.release();
+        entitesTree = null;
+
+        entitiesTreeModel.release();
+        entitiesTreeModel = null;
     }
 
     @Override
     protected void componentOpened() {
         super.componentOpened();
+        Controller controller = CentralLookup.getDefault().lookup(Controller.class);
+        controller.addListener(this);
+        entitiesTreeModel = new EntityTreeModel(controller);
+        entitesTree = new EntitiesTree(controller, entitiesTreeModel);
+
         removeAll();
-        add(new JScrollPane(entitesTree));
+        add(new JScrollPane(entitesTree, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+    }
+
+    @Override
+    public void onControllerEvent(ControllerEventType event) {
+        if (event == ControllerEventType.RELEASE) {
+            close();
+        }
     }
 }
