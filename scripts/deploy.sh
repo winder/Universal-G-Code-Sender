ec848efed8a7a3b7fea7499afb6e7769a78d08b0
Joacim Breiler <joacim@breiler.com>
2022-07-21 13:37:51 +0200

    Fixed wrong file name for 64 bit binary.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ set -xe
 ./jfrog rt u "ugs-classic/target/UniversalGcodeSender.zip" "/UGS/$1/" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/ugs-platform-app-*.zip" "/UGS/$1/ugs-platform-app.zip" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/win-ugs-platform-app-*.zip" "/UGS/$1/ugs-platform-app-win.zip" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
-./jfrog rt u "ugs-platform/application/target/win64-ugs-platform-app-*.zip" "/UGS/$1/ugs-platform-app-win.zip" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
+./jfrog rt u "ugs-platform/application/target/win64-ugs-platform-app-*.zip" "/UGS/$1/ugs-platform-app-win64.zip" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/pi-ugs-platform-app*.tar.gz" "/UGS/$1/ugs-platform-app-pi.tar.gz" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/linux-ugs-platform-app*.tar.gz" "/UGS/$1/ugs-platform-app-linux.tar.gz" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
 ./jfrog rt u "ugs-platform/application/target/ios-ugs-platform-app-*.dmg" "/UGS/$1/ugs-platform-app-ios.dmg" --build-name=$ARTIFACTORY_NAME --build-number=$TRAVIS_BUILD_NUMBER -flat=true
