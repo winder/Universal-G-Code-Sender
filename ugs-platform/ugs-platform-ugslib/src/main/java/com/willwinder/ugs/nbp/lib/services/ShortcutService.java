cf2e7ec472459b621f9d75828aa037300fb499c1
Joacim Breiler <joacim@breiler.com>
2023-02-26 15:43:06 +0100

    Fix problem with the shortcut service that only read default key mappings. It will now listen to the key map settings and update a local cache.

--- ugs-platform/ugs-platform-ugslib/src/main/java/com/willwinder/ugs/nbp/lib/services/ShortcutService.java
+++ ugs-platform/ugs-platform-ugslib/src/main/java/com/willwinder/ugs/nbp/lib/services/ShortcutService.java
@@ -18,31 +18,49 @@
  */
 package com.willwinder.ugs.nbp.lib.services;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import org.apache.commons.lang3.StringUtils;
-import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileAttributeEvent;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
+import org.openide.filesystems.FileRenameEvent;
 import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataShadow;
+import org.openide.loaders.InstanceDataObject;
 import org.openide.util.lookup.ServiceProvider;
 
+import javax.swing.KeyStroke;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 /**
  * A service that can be used to find actions using their shortcut key mapping
- * in the format defined in {@link org.openide.util.Utilities#stringToKey(String)}
+ * in the format defined in {@link org.openide.util.Utilities#keyToString(KeyStroke, boolean)}
+ * (remember to use portable format)
  *
  * @author Joacim Breiler
  */
 @ServiceProvider(service = ShortcutService.class)
-public class ShortcutService {
+public class ShortcutService implements FileChangeListener {
+
+    private static final Logger LOGGER = Logger.getLogger(ShortcutService.class.getSimpleName());
 
     /**
      * Cache the found shortcuts to speed things up using the shortcut as key and the actionId as value.
      */
-    private final Cache<String, String> shortcutCache = CacheBuilder.newBuilder()
-            .expireAfterWrite(20, TimeUnit.SECONDS)
-            .build();
+    private final Map<String, String> shortcutMap = new ConcurrentHashMap<>();
+
+    /**
+     * The current keymap used
+     */
+    private String currentKeymap;
+
+    public ShortcutService() {
+        reloadShortcuts();
+        FileUtil.getSystemConfigRoot().addRecursiveListener(this);
+    }
 
     /**
      * Finds a shortcut in format defined in {@link org.openide.util.Utilities#stringToKey(String)}
@@ -51,19 +69,78 @@ public class ShortcutService {
      * @return a shortcut reference
      */
     public Optional<String> getActionIdForShortcut(String keyAsString) {
-        String actionId = shortcutCache.getIfPresent(keyAsString);
-        if (StringUtils.isNotEmpty(actionId)) {
-            return Optional.of(actionId);
+        return Optional.ofNullable(shortcutMap.get(keyAsString));
+    }
+
+
+    @Override
+    public void fileFolderCreated(FileEvent fileEvent) {
+        // Not used
+    }
+
+    @Override
+    public void fileDataCreated(FileEvent fileEvent) {
+        if (!fileEvent.getFile().getPath().startsWith(getKeymapConfigRoot())) {
+            return;
         }
 
-        FileObject rootFileObject = FileUtil.getConfigFile("Shortcuts/" + keyAsString + ".shadow");
-        if (rootFileObject == null) {
-            return Optional.empty();
+        DataFolder folder = DataFolder.findFolder(FileUtil.getSystemConfigFile(getKeymapConfigRoot()));
+        String keyAsString = fileEvent.getFile().getName();
+        Collections.list(folder.children()).stream()
+                .filter(DataShadow.class::isInstance).map(DataShadow.class::cast)
+                .filter(f -> f.getName().equals(keyAsString))
+                .findFirst()
+                .ifPresent(ShortcutService.this::setShortcut);
+    }
+
+    @Override
+    public void fileChanged(FileEvent fileEvent) {
+        fileDataCreated(fileEvent);
+    }
+
+    @Override
+    public void fileDeleted(FileEvent fileEvent) {
+        if (!fileEvent.getFile().getPath().startsWith("Keymaps/" + currentKeymap + "/")) {
+            return;
         }
 
-        String name = rootFileObject.getName();
-        actionId = rootFileObject.getAttribute("originalFile").toString();
-        shortcutCache.put(name, actionId);
-        return Optional.of(actionId);
+        shortcutMap.remove(fileEvent.getFile().getName());
+        LOGGER.fine(() -> String.format("Removed shortcut: %s", fileEvent.getFile().getName()));
+    }
+
+    @Override
+    public void fileRenamed(FileRenameEvent fileRenameEvent) {
+        // Not used
+    }
+
+    @Override
+    public void fileAttributeChanged(FileAttributeEvent fileAttributeEvent) {
+        if (!fileAttributeEvent.getFile().getPath().equals("Keymaps") && fileAttributeEvent.getName().equals("currentKeymap")) {
+            return;
+        }
+
+        reloadShortcuts();
+    }
+
+    private String getKeymapConfigRoot() {
+        return "Keymaps/" + currentKeymap;
+    }
+
+    private void setShortcut(DataShadow file) {
+        InstanceDataObject cookie = file.getCookie(InstanceDataObject.class);
+        String actionId = cookie.getPrimaryFile().getPath();
+        shortcutMap.put(file.getName(), actionId);
+        LOGGER.fine(() -> String.format("Set shortcut: %s -> %s", file.getName(), actionId));
+    }
+
+    private void reloadShortcuts() {
+        currentKeymap = FileUtil.getConfigFile("Keymaps").getAttribute("currentKeymap").toString();
+        LOGGER.fine(() -> String.format("Reloading shortcuts using keymap %s", currentKeymap));
+
+        shortcutMap.clear();
+        DataFolder folder = DataFolder.findFolder(FileUtil.getSystemConfigFile(getKeymapConfigRoot()));
+        Collections.list(folder.children()).stream()
+                .filter(DataShadow.class::isInstance).map(DataShadow.class::cast)
+                .forEach(this::setShortcut);
     }
 }
