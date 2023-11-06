c9c23b8c90c9913bb482a9430e387f923f423156
Joacim Breiler <joacim@breiler.com>
2023-11-06 19:41:13 +0100

    Added action for creating shortcuts

---
+++ ugs-platform/ugs-platform-ugscore/src/main/java/com/willwinder/ugs/nbp/core/actions/CreateShortcutAction.java
@@ -0,0 +1,93 @@
+package com.willwinder.ugs.nbp.core.actions;
+
+import com.willwinder.ugs.nbp.lib.services.LocalizingService;
+import com.willwinder.universalgcodesender.i18n.Localization;
+import mslinks.ShellLink;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@ActionID(
+        category = LocalizingService.CATEGORY_TOOLS,
+        id = "com.willwinder.ugs.nbp.core.actions.CreateShortcutAction")
+@ActionRegistration(
+        displayName = "resources.MessagesBundle#platform.action.createShortcut",
+        lazy = false)
+@ActionReferences({
+        @ActionReference(
+                path = LocalizingService.MENU_TOOLS,
+                position = 2110)
+})
+public class CreateShortcutAction extends AbstractAction {
+
+    public static final String LINUX_DESKTOP_FILE = System.getProperty("user.home") + "/.local/share/applications/ugs.desktop";
+
+    public CreateShortcutAction() {
+        setEnabled(isEnabled());
+        putValue(NAME, Localization.getString("platform.action.createShortcut"));
+    }
+
+    private static void createLinuxShortcut() {
+        Path currentRelativePath = Paths.get("");
+        String ugsDirectory = currentRelativePath.toAbsolutePath().toString();
+        String desktopFile = "[Desktop Entry]\n" +
+                "Type=Application\n" +
+                "Version=1.0\n" +
+                "Name=Universal Gcode Sender\n" +
+                "Comment=Control your CNC machine\n" +
+                "Path=" + ugsDirectory + "\n" +
+                "Exec=" + ugsDirectory + File.separator + "ugsplatform %F\n" +
+                "Icon=" + ugsDirectory + File.separator + "icon.svg\n" +
+                "MimeType=text/x-gcode\n" +
+                "Terminal=false\n" +
+                "StartupWMClass=Universal Gcode Sender\n" +
+                "Categories=Engineering\n" +
+                "Keywords=ugs\n";
+        try {
+            FileUtils.write(new File(LINUX_DESKTOP_FILE), desktopFile, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (SystemUtils.IS_OS_LINUX) {
+            createLinuxShortcut();
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            createWindowsShortcut();
+        }
+    }
+
+    private static void createWindowsShortcut() {
+        try {
+            String shortcutPath = System.getProperty("user.home") + File.separator + "Desktop";
+            Path currentRelativePath = Paths.get("");
+            String ugsDirectory = currentRelativePath.toAbsolutePath().toString();
+
+            ShellLink.createLink("c:\\" + ugsDirectory + File.separator + "ugsplatform.bat")
+                    .setWorkingDir(ugsDirectory)
+                    .setIconLocation(ugsDirectory + File.separator + "icon.ico")
+                    .setTarget(shortcutPath)
+                    .saveTo("Universal Gcode Sender.lnk");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_WINDOWS;
+    }
+}
