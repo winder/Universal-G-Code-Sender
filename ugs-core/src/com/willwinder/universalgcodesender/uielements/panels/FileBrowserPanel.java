aaa907479e1a44a26f44d795ad07a6649b15a6b3
Andrew D. Murray <andrewmurraydavid@gmail.com>
2024-02-08 01:08:19 -0500

    created FileBrowser Panel and TopComponent

---
+++ ugs-core/src/com/willwinder/universalgcodesender/uielements/panels/FileBrowserPanel.java
@@ -0,0 +1,230 @@
+/*
+    Copyright 2016-2024 Will Winder
+
+    This file is part of Universal Gcode Sender (UGS).
+
+    UGS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UGS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.willwinder.universalgcodesender.uielements.panels;
+
+import com.willwinder.universalgcodesender.listeners.UGSEventListener;
+import com.willwinder.universalgcodesender.model.BackendAPI;
+import com.willwinder.universalgcodesender.model.UGSEvent;
+import com.willwinder.universalgcodesender.utils.GUIHelpers;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeWillExpandListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+/**
+ * Displays a list of files and directories in a given directory.
+ *
+ * @author andrewmurraydavid
+ */
+public final class FileBrowserPanel extends JPanel implements UGSEventListener {
+    private final transient BackendAPI backend;
+    private JTree fileTree;
+    private DefaultTreeModel treeModel;
+    private File currentFile;
+
+    private JTextField currentPathField;
+    private JButton goButton;
+    private JCheckBox showHiddenCheckBox;
+
+    public FileBrowserPanel(BackendAPI backend) {
+        this.backend = backend;
+        setLayout(new BorderLayout());
+
+        JPanel northPanel = new JPanel(new BorderLayout());
+        currentPathField = new JTextField();
+        northPanel.add(currentPathField, BorderLayout.CENTER);
+
+        goButton = new JButton("Go");
+        northPanel.add(goButton, BorderLayout.EAST);
+
+        showHiddenCheckBox = new JCheckBox("Show Hidden", false);
+        northPanel.add(showHiddenCheckBox, BorderLayout.SOUTH);
+
+        add(northPanel, BorderLayout.NORTH);
+
+        File initialDirectory = new File(System.getProperty("user.home"));
+
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(new FileNode(initialDirectory));
+        treeModel = new DefaultTreeModel(rootNode);
+        fileTree = new JTree(treeModel);
+
+        fileTree.setRootVisible(true);
+        fileTree.setShowsRootHandles(true);
+        JScrollPane treeScroll = new JScrollPane(fileTree);
+        add(treeScroll, BorderLayout.CENTER);
+
+        setDirectory(backend != null ?
+                        new File(backend.getSettings().getLastWorkingDirectory()) :
+                        initialDirectory
+                    );
+
+        fileTree.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    TreePath path = fileTree.getPathForLocation(e.getX(), e.getY());
+                    if (path != null) {
+                        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) path.getLastPathComponent();
+                        FileNode fileNode = (FileNode) selectedNode.getUserObject();
+                        if (fileNode.displayName.equals("..")) {
+                            DefaultMutableTreeNode upperNode = (DefaultMutableTreeNode) selectedNode.getParent();
+                            File upperPath = ((FileNode) upperNode.getUserObject()).getFile();
+                            File parentFile = upperPath.getParentFile();
+                            setDirectory(parentFile);
+                        } else if (fileNode.getFile().isDirectory()) {
+                            setDirectory(fileNode.getFile());
+                        } else {
+                            File gcodeFile = fileNode.getFile();
+                            GUIHelpers.openGcodeFile(gcodeFile, backend);
+                        }
+                    }
+                }
+            }
+        });
+
+        fileTree.addTreeWillExpandListener(new TreeWillExpandListener() {
+            @Override
+            public void treeWillExpand(TreeExpansionEvent event) {
+                TreePath path = event.getPath();
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
+                FileNode fileNode = (FileNode) node.getUserObject();
+
+                if (fileNode.isDirectory()) {
+                    node.removeAllChildren();
+                    createChildren(node, fileNode.getFile(), showHiddenCheckBox.isSelected());
+                    ((DefaultTreeModel) fileTree.getModel()).nodeStructureChanged(node);
+                }
+            }
+
+            @Override
+            public void treeWillCollapse(TreeExpansionEvent event) {
+                // No need to handle collapse events in this context
+            }
+        });
+
+        ActionListener goActionListener = e -> changeDirectory(currentPathField.getText());
+        goButton.addActionListener(goActionListener);
+        currentPathField.addActionListener(goActionListener);
+
+        showHiddenCheckBox.addItemListener(e -> {
+            boolean showHidden = e.getStateChange() == ItemEvent.SELECTED;
+            refreshFileList(showHidden);
+        });
+    }
+
+    public void setDirectory(File directory) {
+        currentFile = directory;
+        currentPathField.setText(directory.getAbsolutePath());
+        refreshFileList(showHiddenCheckBox.isSelected());
+        backend.getSettings().setLastWorkingDirectory(directory.getAbsolutePath());
+    }
+
+    private void refreshFileList(boolean showHidden) {
+        DefaultMutableTreeNode newRootNode = new DefaultMutableTreeNode(new FileNode(currentFile));
+        if (currentFile.getParentFile() != null) {
+            newRootNode.add(new DefaultMutableTreeNode(new FileNode(null, "..")));
+        }
+        createChildren(newRootNode, currentFile, showHidden);
+
+        DefaultTreeModel model = (DefaultTreeModel) fileTree.getModel();
+        model.setRoot(newRootNode);
+        model.reload();
+
+        currentPathField.setText(currentFile.getAbsolutePath());
+    }
+
+
+    private void changeDirectory(String path) {
+        File newDirectory = new File(path);
+        if (newDirectory.exists() && newDirectory.isDirectory()) {
+            setDirectory(newDirectory);
+        } else {
+            JOptionPane.showMessageDialog(this, "Directory does not exist: " + path, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void createChildren(DefaultMutableTreeNode node, File file, boolean showHidden) {
+        if (file == null) {
+            return;
+        }
+
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File child : files) {
+                if (!showHidden && (child.isHidden() || child.getName().startsWith(".") || child.getName().startsWith("$"))) {
+                    continue;
+                }
+                DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new FileNode(child));
+                node.add(childNode);
+                if (child.isDirectory()) {
+                    childNode.add(new DefaultMutableTreeNode(new FileNode(null, "Loading...")));
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void UGSEvent(UGSEvent evt) {
+        // React to events as needed
+    }
+
+    private static class FileNode {
+        private final File file;
+        private final String displayName;
+
+        public FileNode(File file) {
+            this.file = file;
+            this.displayName = file.getName().isEmpty() ? file.getPath() : file.getName();
+        }
+
+        public FileNode(File file, String displayName) {
+            this.file = file;
+            this.displayName = displayName;
+        }
+
+        public boolean isDirectory() {
+            return file.isDirectory();
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        @Override
+        public String toString() {
+            return displayName;
+        }
+    }
+}
---
+++ ugs-platform/ugs-platform-ugscore/src/main/java/com/willwinder/ugs/nbp/core/control/FileBrowserTopComponent.java
@@ -0,0 +1,76 @@
+/*
+    Copyright 2016-2024 Will Winder
+
+    This file is part of Universal Gcode Sender (UGS).
+
+    UGS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UGS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UGS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.willwinder.ugs.nbp.core.control;
+
+import com.willwinder.ugs.nbp.lib.Mode;
+import com.willwinder.ugs.nbp.lib.lookup.CentralLookup;
+import com.willwinder.ugs.nbp.lib.services.LocalizingService;
+import com.willwinder.universalgcodesender.model.BackendAPI;
+import com.willwinder.universalgcodesender.uielements.panels.FileBrowserPanel;
+
+import java.awt.BorderLayout;
+
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.windows.TopComponent;
+
+
+/**
+ * Top component which displays something.
+ */
+@TopComponent.Description(
+        preferredID = "FileBrowserTopComponent",
+        //iconBase="SET/PATH/TO/ICON/HERE", 
+        persistenceType = TopComponent.PERSISTENCE_ALWAYS
+)
+@TopComponent.Registration(mode = Mode.LEFT_TOP, openAtStartup = true)
+@ActionID(category = LocalizingService.FileBrowserPanelCategory, id = LocalizingService.FileBrowserPanelActionId)
+@ActionReference(path = LocalizingService.FileBrowserPanelWindowPath)
+@TopComponent.OpenActionRegistration(
+        displayName = "<Not localized:FileBrowserTopComponent>",
+        preferredID = "FileBrowserTopComponent"
+)
+public final class FileBrowserTopComponent extends TopComponent {
+
+    public FileBrowserTopComponent() {
+        this.setLayout(new BorderLayout());
+        BackendAPI backend = CentralLookup.getDefault().lookup(BackendAPI.class);
+        this.add(new FileBrowserPanel(backend), BorderLayout.CENTER);
+    }
+
+    @Override
+    public void componentOpened() {
+        setName(LocalizingService.FileBrowserPanelTitle);
+        setToolTipText(LocalizingService.FileBrowserPanelTooltip);
+    }
+
+    @Override
+    public void componentClosed() {
+        // TODO add custom code on component closing
+    }
+
+    public void writeProperties(java.util.Properties p) {
+        // better to version settings since initial version as advocated at
+        // http://wiki.apidesign.org/wiki/PropertyFiles
+        p.setProperty("version", "1.0");
+    }
+
+    public void readProperties(java.util.Properties p) {
+    }
+}
