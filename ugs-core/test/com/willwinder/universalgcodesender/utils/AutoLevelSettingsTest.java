1485514549c8b9ff3c759adb5fc42e8cf4429882
Joacim Breiler <joacim@breiler.com>
2025-03-28 16:17:50 +0100

    Make sure the z-retract can't be higher than the maximum value

--- ugs-core/test/com/willwinder/universalgcodesender/utils/AutoLevelSettingsTest.java
+++ ugs-core/test/com/willwinder/universalgcodesender/utils/AutoLevelSettingsTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 public class AutoLevelSettingsTest {
 
     @Test
-    public void testAutoLevelSettings() {
+    public void zRetractLowerThanMinShouldDefaultToMin() {
         AutoLevelSettings autoLevelSettings = new AutoLevelSettings();
         autoLevelSettings.setZRetract(-10);
-        assertEquals(0.1, autoLevelSettings.getZRetract(), 0.0001);
+        assertEquals(0.01, autoLevelSettings.getZRetract(), 0.0001);
+    }
+
+    @Test
+    public void zRetractHigherThanMaxShouldDefaultToMin() {
+        AutoLevelSettings autoLevelSettings = new AutoLevelSettings();
+        autoLevelSettings.setZRetract(2);
+        assertEquals(1, autoLevelSettings.getZRetract(), 0.0001);
     }
 }
