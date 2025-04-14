6dd60b0894aa21e782e838be924630b9368d7add
Damian Nikodem <132374191+dnikodem84@users.noreply.github.com>
2025-04-13 22:41:57 -0700

    Code Cleanup for  "Added Depthmap Importer"
    This reverts commit unneeded changes from the commit 148def29f85b27ab6102ebdad8efa746a7fc3868.

--- run_platform.sh
+++ run_platform.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export JAVA_HOME=`/usr/libexec/java_home -v 17`
-"/Applications/Apache NetBeans.app/Contents/Resources/netbeans/java/maven/bin/mvn" install -DskipTests=true && \
-"/Applications/Apache NetBeans.app/Contents/Resources/netbeans/java/maven/bin/mvn" nbm:run-platform -pl ugs-platform/application
+mvn install -DskipTests=true && \
+mvn nbm:run-platform -pl ugs-platform/application
