ed5a1186d4825616b1d5fdb2ea6f1839b47c9806
Joacim Breiler <joacim@breiler.com>
2025-04-25 20:41:32 +0200

    Fix build script and icons

--- ugs-fx/build-linux-x64.sh
+++ ugs-fx/build-linux-x64.sh
@@ -132,4 +132,4 @@ $JAVA_HOME/bin/jpackage \
   --license-file ../COPYING \
   --about-url https://universalgcodesender.com/
 
-mv target/installer/ugs_*.rpm "target/installer/ugs-${APP_VERSION}-x64.rpm"
+mv target/installer/ugs-*.rpm "target/installer/ugs-${APP_VERSION}-x64.rpm"
