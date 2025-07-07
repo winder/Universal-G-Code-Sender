6481d32f6a8265b39b5709e7d9611817ee4cab84
Damian Nikodem <132374191+dnikodem84@users.noreply.github.com>
2025-07-07 01:58:15 -0700

    - Fix issues launching UGS when modern Java installed

--- run_platform.sh
+++ run_platform.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
-mvn install -DskipTests=true && \
-mvn nbm:run-platform -pl ugs-platform/application
+
+export unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     export platform="Linux";;
+    Darwin*)    export platform="Mac";;
+    CYGWIN*)    export platform="Cygwin";;
+    MINGW*)     export platform="MinGw";;
+    MSYS_NT*)   export platform="MSys";;
+    *)          export platform="UNKNOWN:${unameOut}"
+esac
+
+if [[ "${platform}" == "Mac" ]]; then 
+  if [[ -z "${JAVA_HOME}" ]]; then
+    export JAVA_HOME=`/usr/libexec/java_home -v 17` ;
+  fi;
+fi;
+
+/Applications/Apache\ NetBeans.app/Contents/Resources/netbeans/java/maven/bin/mvn install -DskipTests=true && \
+/Applications/Apache\ NetBeans.app/Contents/Resources/netbeans/java/maven/bin/mvn nbm:run-platform -pl ugs-platform/application
