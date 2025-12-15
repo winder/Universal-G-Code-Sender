5d9a0ee34adbb4d45d20b611104288ef6bcd921f
Joacim Breiler <joacim@breiler.com>
2025-12-15 18:20:02 +0100

    Fix icons

--- ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/actions/PrintDesignAction.java
+++ ugs-platform/ugs-platform-plugin-designer/src/main/java/com/willwinder/ugs/nbp/designer/actions/PrintDesignAction.java
@@ -20,11 +20,12 @@ package com.willwinder.ugs.nbp.designer.actions;
 
 import com.willwinder.ugs.nbp.designer.logic.Controller;
 import com.willwinder.ugs.nbp.designer.logic.ControllerFactory;
+import org.openide.util.ImageUtilities;
+
+import javax.swing.JOptionPane;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import org.openide.util.ImageUtilities;
-
 import java.awt.event.ActionEvent;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
@@ -32,17 +33,16 @@ import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
-import javax.swing.JOptionPane;
 
 /**
  * @author Damian Nikodem
  */
 public class PrintDesignAction extends AbstractDesignAction {
-    public static final String SMALL_ICON_PATH = "img/export.svg";
-    public static final String LARGE_ICON_PATH = "img/export24.svg";
-    
+    public static final String SMALL_ICON_PATH = "img/print.svg";
+    public static final String LARGE_ICON_PATH = "img/print24.svg";
+
     private final static double MM_IN_INCH = 0.0393700787401575; // 5 / 127
-    private final static double PRINT_SYSTEM_DPI = 72.0; 
+    private final static double PRINT_SYSTEM_DPI = 72.0;
 
     public PrintDesignAction() {
         putValue("iconBase", SMALL_ICON_PATH);
@@ -51,40 +51,41 @@ public class PrintDesignAction extends AbstractDesignAction {
         putValue("menuText", "Print");
         putValue(NAME, "Print");
     }
+
     private int centerPos(double pageSize, double imageWidth) {
         int result = 0;
-        if ( ( imageWidth / pageSize) < 1.0) {
-            result = (int) ( ( pageSize - imageWidth ) / 2.0 );
-        }        
+        if ((imageWidth / pageSize) < 1.0) {
+            result = (int) ((pageSize - imageWidth) / 2.0);
+        }
         return result;
     }
-    
+
     private Point centerPoint(PageFormat pageFormat, BufferedImage bi) {
-        Point result = new Point (
-            centerPos(pageFormat.getImageableWidth(), bi.getWidth()),
-            centerPos(pageFormat.getImageableHeight(), bi.getHeight())                            
+        return new Point(
+                centerPos(pageFormat.getImageableWidth(), bi.getWidth()),
+                centerPos(pageFormat.getImageableHeight(), bi.getHeight())
         );
-        return result;        
     }
+
     private boolean translateGraphicsForPage(PageFormat pageFormat, BufferedImage bi, int pageIndex, Graphics2D g2d) {
-        int xPageCount = (int)Math.ceil(bi.getWidth() / pageFormat.getImageableWidth());
-        int yPageCount = (int)Math.ceil(bi.getHeight() / pageFormat.getImageableHeight());
+        int xPageCount = (int) Math.ceil(bi.getWidth() / pageFormat.getImageableWidth());
+        int yPageCount = (int) Math.ceil(bi.getHeight() / pageFormat.getImageableHeight());
 
         int totalPages = xPageCount * yPageCount;
 
         if (pageIndex >= totalPages) {
-            return false;//Printable.NO_SUCH_PAGE;
+            return false;
         }
-        
-        int yPageIndex = (int)Math.floor(pageIndex / xPageCount);
+
+        int yPageIndex = (int) Math.floor(pageIndex / xPageCount);
         int xPageIndex = pageIndex % xPageCount;
-        
-        g2d.translate( pageFormat.getImageableX() - (xPageIndex * pageFormat.getImageableWidth() ),
-            pageFormat.getImageableY() - (yPageIndex * pageFormat.getImageableHeight())  );
+
+        g2d.translate(pageFormat.getImageableX() - (xPageIndex * pageFormat.getImageableWidth()),
+                pageFormat.getImageableY() - (yPageIndex * pageFormat.getImageableHeight()));
         // Page is valid
-        return true;      
-    }    
-    
+        return true;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         // Render image first. 
@@ -94,36 +95,35 @@ public class PrintDesignAction extends AbstractDesignAction {
         PrinterJob job = PrinterJob.getPrinterJob();
         double oldScale = controller.getDrawing().getScale();
         Point2D.Double oldPos = controller.getDrawing().getPosition();
-        
+
         // Reset Scale so it draws properly. 
-        controller.getDrawing().setScale(MM_IN_INCH * PRINT_SYSTEM_DPI); 
-                
+        controller.getDrawing().setScale(MM_IN_INCH * PRINT_SYSTEM_DPI);
+
         BufferedImage bi = controller.getDrawing().getImage();
-        
+
         controller.getDrawing().setScale(oldScale);
-        controller.getDrawing().setPosition(oldPos.x,oldPos.y);
-              
+        controller.getDrawing().setPosition(oldPos.x, oldPos.y);
+
         job.setPrintable((Graphics graphics, PageFormat pageFormat, int pageIndex) -> {
-            Graphics2D g2d = (Graphics2D)graphics;
-                        
-            if (!translateGraphicsForPage(pageFormat,bi,pageIndex,g2d)) {
+            Graphics2D g2d = (Graphics2D) graphics;
+
+            if (!translateGraphicsForPage(pageFormat, bi, pageIndex, g2d)) {
                 return Printable.NO_SUCH_PAGE;
             }
-            
-            Point imagePosition = centerPoint(pageFormat, bi);                                                           
-            g2d.drawImage(bi,null, imagePosition.x, imagePosition.y);
-            
+
+            Point imagePosition = centerPoint(pageFormat, bi);
+            g2d.drawImage(bi, null, imagePosition.x, imagePosition.y);
+
             return Printable.PAGE_EXISTS;
         });
-        
+
         if (job.printDialog()) {
             try {
                 job.print();
+            } catch (PrinterException exc) {
+                JOptionPane.showMessageDialog(null, "Error Printing: " + exc.getMessage(), "Error Printing!", JOptionPane.ERROR_MESSAGE);
             }
-            catch (PrinterException exc) {                
-                JOptionPane.showMessageDialog(null, "Error Printing: "+ exc.getMessage(), "Error Printing!", JOptionPane.ERROR_MESSAGE);    
-             }
-         }   
+        }
     }
 
 }
