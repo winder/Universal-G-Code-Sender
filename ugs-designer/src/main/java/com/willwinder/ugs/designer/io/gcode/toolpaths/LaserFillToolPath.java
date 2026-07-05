1c2cb124b6df9aded4146b67b645795853b8cc53
Joacim Breiler <joacim@breiler.com>
2026-07-05 15:18:54 +0200

    Allow using tool path angle on laser fill

--- ugs-designer/src/main/java/com/willwinder/ugs/designer/io/gcode/toolpaths/LaserFillToolPath.java
+++ ugs-designer/src/main/java/com/willwinder/ugs/designer/io/gcode/toolpaths/LaserFillToolPath.java
@@ -6,15 +6,15 @@ import com.willwinder.ugs.designer.io.gcode.path.GcodePath;
 import com.willwinder.ugs.designer.io.gcode.path.Segment;
 import com.willwinder.ugs.designer.io.gcode.path.SegmentType;
 import com.willwinder.ugs.designer.model.Settings;
+import static com.willwinder.ugs.designer.utils.GeometryUtils.generateLineString;
 import com.willwinder.universalgcodesender.model.PartialPosition;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.CoordinateXY;
 import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.MultiPoint;
 
 import java.awt.geom.Area;
+import java.util.Arrays;
 import java.util.List;
 
 public class LaserFillToolPath extends AbstractToolPath {
@@ -37,28 +37,55 @@ public class LaserFillToolPath extends AbstractToolPath {
     public void appendGcodePath(GcodePath gcodePath, Settings settings) {
         gcodePath.addSegment(new Segment(SegmentType.SEAM, null, null, (int) Math.round(settings.getMaxSpindleSpeed() * (source.getSpindleSpeed() / 100d)), source.getFeedRate()));
 
+        double toolPathAngle = source.getToolPathAngle();
         List<Geometry> geometries = getGeometries();
         geometries.forEach(g -> {
             Envelope envelope = g.getEnvelopeInternal();
+            double[] offsetRange = getOffsetRange(envelope, toolPathAngle);
 
             int currentPass = 0;
             while (currentPass < source.getPasses()) {
                 currentPass++;
 
                 boolean reverse = false;
-                double currentY = envelope.getMinY();
-                while (currentY <= envelope.getMaxY()) {
-                    LineString lineString = getGeometryFactory().createLineString(new Coordinate[]{
-                            new CoordinateXY(envelope.getMinX(), currentY),
-                            new CoordinateXY(envelope.getMaxX(), currentY),
-                    });
-
-                    addLineIntersectionSegments(gcodePath, g, lineString, reverse);
-                    currentY += settings.getLaserDiameter();
+                double currentOffset = offsetRange[0];
+                while (currentOffset <= offsetRange[1]) {
+                    LineString lineString = generateLineString(envelope, currentOffset, toolPathAngle);
+                    if (lineString != null) {
+                        addLineIntersectionSegments(gcodePath, g, lineString, reverse);
+                    }
+                    currentOffset += settings.getLaserDiameter();
                     reverse = !reverse;
                 }
             }
         });
+    }
+
+    /**
+     * Returns the range of offsets along the pass normal that covers the whole envelope for the given angle.
+     * The offset is measured from the envelope's minimum corner, matching {@link com.willwinder.ugs.designer.utils.GeometryUtils#generateLineString}.
+     */
+    private static double[] getOffsetRange(Envelope envelope, double angleInDegrees) {
+        double radians = Math.toRadians(-angleInDegrees);
+        double dx = Math.cos(radians);
+        double dy = Math.sin(radians);
+
+        double normalX = -dy;
+        double normalY = dx;
+
+        double width = envelope.getWidth();
+        double height = envelope.getHeight();
+
+        double[] projections = {
+                0,
+                width * normalX,
+                height * normalY,
+                width * normalX + height * normalY
+        };
+
+        double min = Arrays.stream(projections).min().orElse(0);
+        double max = Arrays.stream(projections).max().orElse(0);
+        return new double[]{min, max};
     }
 
     private static void addLineIntersectionSegments(GcodePath gcodePath, Geometry geometry, LineString lineString, boolean reverse) {
